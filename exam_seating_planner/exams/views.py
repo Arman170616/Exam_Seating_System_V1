77b54167ecc224f75264253a8cdacda27e1065f7
Arman170616 <armanice09@gmail.com>
2024-05-22 09:18:35 +0600

    up to 22May

--- exam_seating_planner/exams/views.py
+++ exam_seating_planner/exams/views.py
@@ -76,7 +76,8 @@ def upload_file(request):
 # FINAL OUTPUT
 
 
-'''
+
+
 
 import pandas as pd
 from openpyxl import Workbook
@@ -89,164 +90,15 @@ import os
 # Read the data from the Excel file
 # df = pd.read_excel('TestData_BETA.xlsx')
 try:
-    df = pd.read_excel('TestData_8May5.xlsx')
+    df = pd.read_excel('IS-22May.xlsx')
 except FileNotFoundError:
-    print("Failed to find 'TestData_8May5.xlsx'. Please check the file path.")
+    print("Failed to find '20May_M.xlsx'. Please check the file path.")
     raise
 
 try:
     img = Image("British_Council_Logo.png")
 except FileNotFoundError:
-    print("Failed to find 'British_Council_Logo.png'. Please check the file path.")
-    raise
-
-# Function to generate exam desk cards
-def generate_exam_desk_cards(data):
-    # Group students by exam venue, exam room, session, board, and date
-    grouped_data = data.groupby(['Board', 'Venue2', 'Exam Room', 'Session', 'Date'])
-
-    # Define colors for each unique paper code
-    colors = [PatternFill(start_color='FFC7CE', end_color='FFC7CE', fill_type='solid'),
-              PatternFill(start_color='FFEB9B', end_color='FFEB9B', fill_type='solid'),
-              PatternFill(start_color='C9FFC3', end_color='C9FFC3', fill_type='solid'),
-              PatternFill(start_color='9BEBFF', end_color='9BEBFF', fill_type='solid'),
-              PatternFill(start_color='FFC3E8', end_color='FFC3E8', fill_type='solid'),
-              PatternFill(start_color='B8B8FF', end_color='B8B8FF', fill_type='solid'),
-              PatternFill(start_color='D8BFD8', end_color='D8BFD8', fill_type='solid'),
-              PatternFill(start_color='C0C0C0', end_color='C0C0C0', fill_type='solid'),
-              PatternFill(start_color='F5DEB3', end_color='F5DEB3', fill_type='solid'),
-              PatternFill(start_color='FFD700', end_color='FFD700', fill_type='solid'),
-              PatternFill(start_color='808080', end_color='808080', fill_type='solid'),
-              PatternFill(start_color='FFA07A', end_color='FFA07A', fill_type='solid'),
-              PatternFill(start_color='FFFACD', end_color='FFFACD', fill_type='solid'),
-              PatternFill(start_color='7B68EE', end_color='7B68EE', fill_type='solid'),
-              PatternFill(start_color='FF6347', end_color='FF6347', fill_type='solid')]
-
-    # Iterate over each group and generate exam desk cards
-    for (board, venue, exam_room, session, date), group in grouped_data:
-        date_str = date.strftime("%Y-%m-%d")
-        directory_path = os.path.join('Exam_Seat_planner', board, venue, date_str)
-        os.makedirs(directory_path, exist_ok=True)
-
-        # Define file path
-        file_name = f'Seat_Plan_{venue}_{exam_room}_{session}_{date_str}.xlsx'
-        file_path = os.path.join(directory_path, file_name)
-
-
-        # Create a workbook
-        wb = Workbook()
-        ws = wb.active
-
-        # Add logo to the worksheet with adjusted position
-        # img = Image("British_Council_Logo.png")
-        img.width = 150  # Adjust the width of the image (optional)
-        img.height = 80  # Adjust the height of the image (optional)
-        ws.add_image(img, 'A1')  # Set the anchor to 'A1' to position the image within the cell
-
-        # Add headers and other details
-        ws['A5'] = f"{board} Examinations - {date.strftime('%Y-%m-%d')}"
-        ws['A6'] = f"Venue: {venue}, Exam Room: {exam_room}"
-        ws['A7'] = f"Session - {session}"
-        ws['A8'] = f"Date: {date.strftime('%Y-%m-%d')}"
-
-        paper_codes = ', '.join(map(str, group['Paper code'].unique()))
-        ws['A9'] = f"Subject: {paper_codes}"
-
-        # Add total candidate count
-        ws.merge_cells('A10:B10')
-        ws['A10'] = "Total Candidate"
-        ws['A10'].alignment = Alignment(horizontal='center', vertical='center')
-        ws['C10'] = len(group)
-
-        # Initialize variables for row and column numbers
-        start_row = 0
-        start_col = 1
-
-        # Initialize color index
-        color_index = 0
-
-        # Initialize color dictionary
-        color_dict = {}
-
-        # Iterate over candidate numbers and assign them to cells based on seat locator
-        for _, row_data in group.iterrows():
-            candidate_num = row_data['Candidate Number']
-            seat_locator = row_data['Seat Locator']
-            paper_code = row_data['Paper code']
-
-            if pd.notnull(seat_locator):
-                # Convert seat locator to row and column indices
-                col = ord(seat_locator[0].lower()) - ord('a') + start_col  # Convert column letter to index
-                row = int(seat_locator[1:]) + start_row  # Convert row number to index
-
-                # Write candidate number to the specified cell
-                ws.cell(row=row, column=col).value = candidate_num
-
-                # Add border to the cell
-                ws.cell(row=row, column=col).border = Border(left=Side(style='thin'),
-                                                              right=Side(style='thin'),
-                                                              top=Side(style='thin'),
-                                                              bottom=Side(style='thin'))
-
-                # Assigning color to the cell based on the paper code
-                if paper_code not in color_dict:
-                    color_dict[paper_code] = colors[color_index]
-                    color_index = (color_index + 1) % len(colors)
-
-                # Applying fill color to the cell
-                ws.cell(row=row, column=col).fill = color_dict[paper_code]
-
-        # Add total candidate count for each paper code
-        for i, paper_code in enumerate(group['Paper code'].unique(), start=11):
-            count = len(group[group['Paper code'] == paper_code])
-            if count > 0:
-                # Write total count for each paper code 
-                ws.merge_cells(start_row=i, start_column=start_col, end_row=i, end_column=start_col + 1)
-                ws.cell(row=i, column=start_col).value = f"Paper Code {paper_code}"
-                ws.cell(row=i, column=start_col).font = Font(size=8)
-                ws.cell(row=i, column=start_col).alignment = Alignment(horizontal='center')
-                ws.cell(row=i, column=start_col + 2).value = count
-                # Applying fill color to the cell
-                ws.cell(row=i, column=start_col).fill = color_dict[paper_code]
-
-        # Calculate the maximum number of columns needed based on the length of 'Seat Locator' values
-        max_column = max([ord(seat_locator[0].lower()) - ord('a') + 1 for seat_locator in group['Seat Locator'] if pd.notnull(seat_locator)], default=0)
-
-        # Add column numbers dynamically starting from cell B14
-        for col_num in range(start_col, start_col + max_column):
-            ws.cell(row=17, column=col_num).value = f"Column {col_num}"
-
-        # # Save the workbook
-        wb.save(file_path)
-        print(f"Successfully saved as: {file_path}")
-
-
-# # Generate exam desk cards
-generate_exam_desk_cards(df)
-
-
-'''
-
-import pandas as pd
-from openpyxl import Workbook
-from openpyxl.drawing.image import Image
-from openpyxl.styles import Font, Border, Side, PatternFill, Alignment
-from openpyxl.utils import get_column_letter
-import os
-
-
-# Read the data from the Excel file
-# df = pd.read_excel('TestData_BETA.xlsx')
-try:
-    df = pd.read_excel('TestData14MayV1.xlsx')
-except FileNotFoundError:
-    print("Failed to find 'TestData14MayV1.xlsx'. Please check the file path.")
-    raise
-
-try:
-    img = Image("British_Council_Logo.png")
-except FileNotFoundError:
-    print("Failed to find 'British_Council_Logo.png'. Please check the file path.")
+    print("Failed to find 'British_Council_Logo.png'. Please check the file path.") 
     raise
 
 # Function to generate exam desk cards
@@ -373,3 +225,105 @@ def generate_exam_desk_cards(data):
 generate_exam_desk_cards(df)
 
 
+
+
+# import pandas as pd
+# import os
+# import logging
+# import pdfkit
+# from xlsxwriter import Workbook
+
+# logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# try:
+#     df = pd.read_excel('20May_M.xlsx')
+# except FileNotFoundError:
+#     logging.error("Failed to find '20May_M.xlsx'. Please check the file path.")
+#     raise
+
+# try:
+#     logo_path = "British_Council_Logo.png"
+#     if not os.path.exists(logo_path):
+#         raise FileNotFoundError
+# except FileNotFoundError:
+#     logging.error("Failed to find 'British_Council_Logo.png'. Please check the file path.")
+#     raise
+
+# colors = ['FFC7CE', 'FFEB9B', 'C9FFC3', '9BEBFF', 'FFC3E8', 'B8B8FF', 'D8BFD8',
+#           'C0C0C0', 'F5DEB3', 'FFD700', '808080', 'FFA07A', 'FFFACD', '7B68EE', 'FF6347']
+
+# def generate_exam_desk_cards(data):
+#     grouped_data = data.groupby(['Venue2', 'Exam Room', 'Session', 'Date'])
+
+#     for (venue, exam_room, session, date), group in grouped_data:
+#         try:
+#             date_str = date.strftime("%Y-%m-%d")
+#             directory_path = os.path.join('Exam_Seat_planner', venue, date_str)
+#             os.makedirs(directory_path, exist_ok=True)
+
+#             file_name = f'Seat_Plan_{venue}_{exam_room}_{session}_{date_str}.xlsx'
+#             file_path = os.path.join(directory_path, file_name)
+
+#             # Create a workbook and worksheet
+#             wb = Workbook(file_path)
+#             ws = wb.add_worksheet()
+
+#             # Add headers and other details
+#             ws.set_row(0, 60)
+#             ws.insert_image('A1', logo_path, {'x_scale': 0.5, 'y_scale': 0.5})
+#             ws.write('A5', "Pearson Edexcel & Cambridge International Examination May/June 2024")
+#             ws.write('A6', f"Venue: {venue}, Exam Room: {exam_room}")
+#             ws.write('A7', f"Session - {session}")
+#             ws.write('A8', f"Date: {date_str}")
+
+#             paper_codes = ', '.join(map(str, group['Paper code'].unique()))
+#             ws.write('A9', f"Subject: {paper_codes}")
+
+#             ws.merge_range('A10:B10', "Total Candidate")
+#             ws.write('C10', len(group))
+
+#             start_row = 0
+#             start_col = 1
+
+#             color_index = 0
+#             color_dict = {}
+
+#             for _, row_data in group.iterrows():
+#                 candidate_num = row_data['Candidate Number']
+#                 seat_locator = row_data['Seat Locator']
+#                 paper_code = row_data['Paper code']
+
+#                 if pd.notnull(seat_locator):
+#                     col = ord(seat_locator[0].lower()) - ord('a') + start_col
+#                     row = int(seat_locator[1:]) + start_row
+
+#                     cell_format = wb.add_format({'border': 1})
+#                     if paper_code not in color_dict:
+#                         color_dict[paper_code] = colors[color_index]
+#                         color_index = (color_index + 1) % len(colors)
+
+#                     cell_format.set_bg_color(color_dict[paper_code])
+#                     ws.write(row, col, candidate_num, cell_format)
+
+#             wb.close()
+
+#             html_file_path = file_path.replace('.xlsx', '.html')
+#             pdf_file_path = file_path.replace('.xlsx', '.pdf')
+
+#             df.to_html(html_file_path, index=False)
+
+#             options = {
+#                 'page-size': 'A4',
+#                 'orientation': 'Portrait',
+#                 'zoom': '0.85'
+#             }
+
+#             pdfkit.from_file(html_file_path, pdf_file_path, options=options)
+#             os.remove(html_file_path)
+
+#             logging.info(f"Successfully saved as: {pdf_file_path}")
+
+#         except Exception as e:
+#             logging.error(f"Failed to process group {(venue, exam_room, session, date)}: {e}")
+
+# generate_exam_desk_cards(df)
